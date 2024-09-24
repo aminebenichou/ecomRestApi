41011d2fca89e69d89b950d9fd3376cbf7d1f8de
Benichou Mohammed El Amine <31164395+aminebenichou@users.noreply.github.com>
2024-09-24 15:59:28 +0100

    examples

--- ecomapi/ecomapi/urls.py
+++ ecomapi/ecomapi/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from ouedkniss import views
+from rest_framework import routers
+
+# router = routers.DefaultRouter()
+# router.register('info', views.Home)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    # path('', views.home)
+    # path('clients/', include(router.urls)),
+] 
+
--- ecomapi/ouedkniss/views.py
+++ ecomapi/ouedkniss/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import *
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 # Create your views here.
 # abcdefgh
 # hello
+
+# @api_view(['GET'])
+# def home(request):
+#     return Response({'msg':'hello'}, status=HTTP_200_OK)
+
+
+# class Home(viewsets.ModelViewSet):
+#     queryset = Client.objects.all()
+#     serializer_class = ClientSerializer
+#     def list(self, request, *args, **kwargs):
+#         result = Client.objects.all()
+#         print(result)
+#         resulttwo = []
+#         for item in result:
+#             resulttwo.append({
+#                 'id':item.id,
+#                 'name':item.name,
+#                 'total orders':'hello'
+#             })
+#         return Response(resulttwo, status=HTTP_200_OK)
+        
+
